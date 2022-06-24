462b3bec0df2b12dc5c47fc3c6f5695ddc442673
brummhummel <108004811+brummhummel@users.noreply.github.com>
2022-06-24 17:39:59 +0200

    Update wishlist.php

--- src/wishlist.php
+++ src/wishlist.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Amazon Wish Lister
  *
@@ -12,26 +11,22 @@
 error_reporting(E_ERROR);
 set_time_limit(300);
 require_once('phpquery.php');
-
 //?id=YOUR_AMAZON_ID
 //get the amazon id or force an ID if none is passed
 if(isset($_GET['id'])) $amazon_id = $_GET['id'];
 else $amazon_id = '37XI10RRD17X2';
-
 //?tld=AMAZON_COUNTRY
 //Set the regional variant of Amazon to use.  e.g `?tld=co.uk` or `?tld=de` or ?tld=com`. Defaults to `com`
 //Tested with: `ca`, `com`, `com.br`, `co.jp`, `co.uk`, `de`, `fr`, `in`, `it`
 //Currently no wishlists available for: `com.au`, `com.mx`, `es`, `nl`
 if(isset($_GET['tld'])) $amazon_country = $_GET['tld'];
 else $amazon_country = 'com';
-
 //?reveal=unpurchased
 //checks what to reveal (unpurchased, all, or purchased)... defaults to unpurchased
 if($_GET['reveal'] == 'unpurchased') $reveal = 'reveal=unpurchased';
 elseif($_GET['reveal'] == 'all') $reveal = 'reveal=all';
 elseif($_GET['reveal'] == 'purchased') $reveal = 'reveal=purchased';
 else $reveal = 'reveal=unpurchased';
-
 //?sort=date
 //sorting options (date, title, price-high, price-low, updated, priority)
 if($_GET['sort'] == 'date') $sort = 'sort=date-added';
@@ -41,16 +36,13 @@ elseif($_GET['sort'] == 'price-low') $sort = 'sort=universal-price';
 elseif($_GET['sort'] == 'price-high') $sort = 'sort=universal-price-desc';
 elseif($_GET['sort'] == 'updated') $sort = 'sort=last-updated';
 else $sort = 'sort=date-added';
-
 //?tag=affiliate-tag
 //Set the affiliate tag - usually ends `-21`
 if(isset($_GET['tag'])) $affiliate_tag = $_GET['tag'];
 else $affiliate_tag = '';
-
 $baseurl = 'http://www.amazon.' . $amazon_country;
 $content = phpQuery::newDocumentFile("$baseurl/registry/wishlist/$amazon_id?$reveal&$sort&layout=standard");
 $i = 0;
-
 if($content == '')
 {
 	echo('ERROR');
@@ -74,8 +66,12 @@ else
 	if(empty($pages)) $pages=1;
 
 	for($page_num=1; $page_num<=$pages; $page_num++)
+    $lek = '';
+
+	do
 	{
 		$contents = phpQuery::newDocumentFile("$baseurl/registry/wishlist/$amazon_id?$reveal&$sort&layout=standard&page=$page_num");
+		$contents = phpQuery::newDocumentFile("$baseurl/registry/wishlist/$amazon_id?$reveal&$sort&layout=standard&lek=$lek");
 
 		if($contents == '')
 		{
@@ -86,7 +82,6 @@ else
 		{
 			//get all items
 			$items = pq('tbody.itemWrapper');
-
 			//if items exist (the let's use the old Amazon wishlist
 			if($items->html())
 			{
@@ -94,7 +89,6 @@ else
 				foreach($items as $item)
 				{
 					$check_if_regular = pq($item)->find('span.commentBlock nobr');
-
 					if($check_if_regular != '')
 					{
 						//$array[$i]['array'] = pq($item)->html();
@@ -119,7 +113,6 @@ else
 						if($_GET['author'] == true) {
 							$array[$i]['author'] = get_Author($array[$i]['link']);
 						}
-
 						$i++;
 					}
 				}
@@ -129,19 +122,16 @@ else
 			else
 			{
 				$items = pq('.g-items-section div[id^="item_"]');
-
 				//loop through items
 				foreach($items as $item)
 				{
 					$name = htmlentities(trim(pq($item)->find('a[id^="itemName_"]')->html()));
 					$link = pq($item)->find('a[id^="itemName_"]')->attr('href');
-
 					if(!empty($name) && !empty($link))
 					{
 						$total_ratings = pq($item)->find('div[id^="itemInfo_"] div:a-spacing-small:first a.a-link-normal:last')->html();
 						$total_ratings = trim(str_replace(array('(', ')'), '', $total_ratings));
 						$total_ratings = is_numeric($total_ratings) ? $total_ratings : '';
-
 						//$array[$i]['array'] = pq($item)->html();
 						$array[$i]['num'] = $i + 1;
 						$array[$i]['name'] = $name;
@@ -150,8 +140,8 @@ else
 						$array[$i]['new-price'] = text_prepare(pq($item)->find('span[id^="itemPrice_"]')->html());
 						$array[$i]['date-added'] = text_prepare(str_replace('Added', '', pq($item)->find('div[id^="itemAction_"] .a-size-small')->html()));
 						$array[$i]['priority'] = text_prepare(pq($item)->find('span[id^="itemPriorityLabel_"]')->html());
-						$array[$i]['rating'] = 'N/A';
-						$array[$i]['total-ratings'] = $total_ratings;
+						
+						
 						$array[$i]['comment'] = text_prepare((pq($item)->find('span[id^="itemComment_"]')->html()));
 						$array[$i]['picture'] = pq($item)->find('div[id^="itemImage_"] img')->attr('src');
 						$array[$i]['page'] = $page_num;
@@ -166,13 +156,18 @@ else
 							$array[$i]['author']->find('h5')->remove();
 							$array[$i]['author'] = trim(preg_replace('/\([\ \w]+\)/', '', str_replace('by', '', $array[$i]['author']->text())));
 						}
-
 						$i++;
-					}
+					}				       
 				}
+				
+				$next_page = pq('script[data-a-state=\'{"key":"scrollState"}\']')->html();
+				preg_match("/\"lastEvaluatedKey\":\"(.*)\"/", $next_page, $next_page_url);
+				$lek = $next_page_url[1];
 			}
 		}
 	}
+
+	while (!$lek == '');
 }
 
 //go to product details page for isbn
@@ -180,12 +175,10 @@ function get_ISBN($url) {
 	$productPage = phpQuery::newDocumentFile($url);
 	return trim(str_replace("ISBN-13:", "", text_prepare(pq($productPage)->find('.bucket .content li:has(b:contains("ISBN-13"))')->text())));
 }
-
 function get_Author($url) {
 	$productPage = phpQuery::newDocumentFile($url);
 	return trim(str_replace('(Author)', '', text_prepare(pq($productPage)->find('#byline .author .a-popover-preload .a-size-medium')->text())));
 }
-
 //format the xml (old style)
 function xml_ecode($array) {
 	$xml = '';
@@ -194,14 +187,12 @@ function xml_ecode($array) {
 			if (is_numeric($key)) {
 				$key = 'item';
 			}
-
 			//create the xml tags
 			$xml .= '<' . $key . '>' . xml_ecode($value) . '</' . $key . '>';
 		}
 	} else { $xml = htmlspecialchars($array, ENT_QUOTES); }
 	return $xml;
 }
-
 //Convert an array into valid XML
 //From http://stackoverflow.com/a/5965940/1127699
 function xml_encode($data, &$xml_data) {
@@ -219,20 +210,14 @@ function xml_encode($data, &$xml_data) {
 			$xml_data->addChild("$key",htmlspecialchars("$value"));
 		}
 	}
-
 	return $xml_data->asXML();
 }
-
 function rss_encode($data) {
-
 	global $baseurl;
-
 	//	Most recent item
 	//	Should really be RFC-822
 	$pubDate = $data[0]['date-added'];
-
 	$link = htmlspecialchars("{$baseurl}/registry/wishlist/{$amazon_id}?{$reveal}&{$sort}&layout=standard");
-
 	$rss = '<?xml version="1.0" encoding="UTF-8" ?>
 			<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 				<channel>
@@ -241,7 +226,6 @@ function rss_encode($data) {
 					<link>'.$link.'</link>
 					<language>en-gb</language>
 					<pubDate>'.$pubDate.'</pubDate>';
-
 	foreach( $data as $key => $value )
 	{
 		if( is_array($value) )
@@ -263,18 +247,14 @@ function rss_encode($data) {
 					</item>';
 		}
 	}
-
 	$rss .= '</channel>
 			</rss>';
-
 	return $rss;
 }
-
 //Make sure the text is prepared for use on the web.
 function text_prepare($text) {
 	return trim($text);
 }
-
 //	Get the ASIN of the product
 function get_ASIN($url) {
 	/*
@@ -291,54 +271,39 @@ function get_ASIN($url) {
 		http://www.amazon.com.br/dp/B00YSILJZU/ref=wl_it_dp_v_nS_ttl/177-3586976-1287133?_encoding=UTF8&colid=3OF5TPV1ZMWLM&coliid=I36RLGSQVQOUYH
 		http://www.amazon.fr/dp/B003FRXFWK/ref=wl_it_dp_v_S_ttl/275-1202652-6343230?_encoding=UTF8&colid=1EHE58O7QKWTH&coliid=I3EGD6MPOTC9GY
 	*/
-
 	//	Remove the Base URL and /dp/
 	global $baseurl;
 	$ASIN = str_replace($baseurl . "/dp/", '', $url);
 	//	Grab the ASIN
 	$ASIN = substr($ASIN, 0, 10);
-
 	return $ASIN;
 }
-
 function get_large_ssl_image($image_url) {
 	/*
 		Change
 			http://ecx.images-amazon.com/images/I/41kWB4Z4PTL._SL250_.jpg
 		To
 			https://images-eu.ssl-images-amazon.com/images/I/41kWB4Z4PTL._SL2500_.jpg
-
 		Image URLs are always .com for some reason.
 	*/
-
 	$largeSSLImage = str_replace("http://ecx.images-amazon.com", 'https://images-eu.ssl-images-amazon.com', $image_url);
 	$largeSSLImage = str_replace("_.jpg", '0_.jpg', $largeSSLImage);
-
 	return $largeSSLImage;
 }
-
 function get_affiliate_link($AISN) {
-
 	/*
 		According to https://affiliate-program.amazon.co.uk/gp/associates/help/t5/a21
-
 		> So if you need to build a simple text link to a specific item on Amazon.co.uk, here is the link format you need to use:
 		http://www.amazon.co.uk/dp/ASIN/ref=nosim?tag=YOURASSOCIATEID
-
 		e.g. http://www.amazon.co.uk/dp/B00U7EXH72/ref=nosim?tag=shkspr-21
-
 		Is this the same for all countries?
-
 		Your Associate ID only workds with one country
 		https://affiliate-program.amazon.co.uk/gp/associates/help/t22/a13%3Fie%3DUTF8%26pf_rd_i%3Dassoc_he..
 	*/
-
 	global $baseurl, $affiliate_tag;
 	$affiliateURL = $baseurl . "/dp/" . $AISN . "/ref=nosim?tag=" . $affiliate_tag;
-
 	return $affiliateURL;
 }
-
 //?format=json
 //format the wishlist (json, xml, or php array object) defaults to json
 if($_REQUEST['format'] == 'json') {
